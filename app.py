f18ddaa8304c77a6a0061a9a392f2eee6e0edadc
pakitowka <whitesaint12@gmail.com>
2023-06-07 12:55:13 +0200

    refactor index.html

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = ""
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
+
     return render_template("index.html")
 
 
